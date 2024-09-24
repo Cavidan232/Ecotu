4db72a8c2353d3370fe5ef2424a10092fcf9aa16
cavidan-1907 <cavidanvelizade9@gmail.com>
2024-09-25 02:05:35 +0400

    start project

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
-import Footer from '../Components/Footer'
-;import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 function Layout() {
   return (
     <div>
       <Navbar />
       <main>   <Outlet />
       </main>
-      <Footer></Footer>
     </div>
   );
 }
